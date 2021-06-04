5d674a2994c67c495ad348d16556445894eaad48
Mathis <handymathis@gmail.com>
2021-06-04 12:47:49 +0200

    added about page

---
+++ webapp/src/routes/about.svelte
@@ -0,0 +1,16 @@
+<div>
+	<Navbar />
+	<div class='container'>
+		Content
+	</div>
+</div>
+
+<script lang="ts">
+	import Navbar from "../lib/navbar.svelte";
+</script>
+
+<style lang='scss'>
+	.container {
+    margin-top: 10px;
+	}
+</style>
