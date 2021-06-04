e0ef54225f6a5a95a5f6b8755129b62ca6a33d3d
Mathis <handymathis@gmail.com>
2021-06-04 12:10:24 +0200

    added style to navbar

--- webapp/src/lib/navbar.svelte
+++ webapp/src/lib/navbar.svelte
@@ -1,5 +1,9 @@
 <div class='navbar'>
-
+	<div class='navbar-menu'>
+		<a>Home</a>
+		<a>About</a>
+		<a>privacy</a>
+	</div>
 </div>
 
 
@@ -9,5 +13,24 @@
 		height: 70px;
 		background: #2e2d35;
 		border-radius: 10px;
+	}
+	.navbar-menu {
+		width: 100%;
+		height: 100%;
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		align-items: center;
+		gap: 20px;
+	}
+	.navbar-menu a {
+		color: white;
+		font-family: "Roboto", sans-serif;
+		font-size: 1.4em;
+		cursor: pointer;
+		transition: .3s;
+	}
+	.navbar-menu a:hover {
+		color: #acacac;
 	}
 </style>
