ce4f2485861253b81f3fa3a5188f368ff470944d
Mathis <handymathis@gmail.com>
2021-06-06 20:26:54 +0200

    added snackbar for quick message alerts

---
+++ webapp/src/lib/snackbar.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+	let show = false;
+	export let text: string;
+	export let color: string;
+	export let timeout: number;
+
+	export function showMessage() {
+		show = true;
+		setTimeout(() => {
+			show = false;
+		}, timeout);
+	}
+</script>
+
+{#if show}
+	<div class='snackbar' style={`background: ${color};`}>
+		{text}
+	</div>
+{/if}
+
+<style lang='scss'>
+	.snackbar {
+		position: absolute;
+		z-index: 20;
+		width: 200px;
+		height: 50px;
+		bottom: 10px;
+		left: 50%;
+		transform: translateX(-50%);
+		text-align: center;
+		color: white;
+		font-size: 1.5em;
+		display: grid;
+		place-items: center;
+		font-family: "Roboto", sans-serif;
+		border-radius: 5px;
+		animation-name: slideIn;
+		animation-duration: 0.6s;
+		animation-fill-mode: forwards;
+	}
+
+	@keyframes slideIn {
+		0% {
+			bottom: -50px;
+		}
+		100% {
+			bottom: 10px;
+		}
+  }
+</style>
--- webapp/src/routes/index.svelte
+++ webapp/src/routes/index.svelte
@@ -2,14 +2,57 @@
 	<title>Shorty</title>
 </svelte:head>
 
-<div>
-	<Navbar />
-</div>
-
 <script lang="ts">
 	import Navbar from "../lib/navbar.svelte";
+	import Snackbar from "../lib/snackbar.svelte";
+
+	let snackbar;
+	let snackbarText: string;
+	let inputValue: string = '';
+
+	function onSubmit(e: KeyboardEvent): void {
+		if (e.key === "Enter") {
+			snackbar.showMessage();
+		}
+	}
+
+	function validateUrl(url: string): boolean {
+		return false;
+	}
 </script>
 
-<style lang='scss'>
+<div>
+	<Navbar />
+	<div class='container'>
+		<input type='text' on:keydown={e => onSubmit(e)} value={inputValue}>
+	</div>
+</div>
 
+<Snackbar color='green' timeout={2000} bind:this={snackbar} text={snackbarText} />
+
+<style lang='scss'>
+	.container {
+    display: grid;
+    place-items: center;
+		width: 100vw;
+		height: 100vh;
+  }
+	.container input {
+		padding: 20px 170px;
+		background: #2e2d35;
+		border: 2px solid #232228;
+		border-radius: 10px;
+		outline: none;
+		font-size: 1.5em;
+		color: white;
+		transition: .3s;
+		text-align: center;
+	}
+	.container input:hover {
+		filter: brightness(1.2);
+	}
+	.container input:focus {
+    box-shadow: 2px 2px 2px rgba(0,0,0,0.6);
+    transform: scale(1.02);
+	}
 </style>
